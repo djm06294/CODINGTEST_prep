4d021740077ed53f1cc2332374738d4197096b50
djm06294 <jykimjbr860@gmail.com>
2024-01-25 23:24:19 +0900

    0125_A041_#23

--- 2nd_submission/A041_김주영_20240125.cpp
+++ 2nd_submission/A041_김주영_20240125.cpp
@@ -10,7 +10,29 @@
 using namespace std;
 
 string solution(string s) {
-    for_each(s.begin()+1, s.end(), [](auto& c){c = tolower(c);});
+    for_each(s.begin(), s.end(), [](auto& c){c = tolower(c);});
     s[0] = toupper(s[0]);
+
+    int len = s.length();
+    for(int i=0; i<len-1; i++)
+        if(s[i] == ' ') s[i+1] = toupper(s[i+1]);
+
     return s;
 }
+
+
+// 윤원준's code
+// #include <string>
+// #include <vector>
+
+// using namespace std;
+
+// string solution(string s) {
+//     string answer = "";
+
+//     answer += toupper(s[0]);
+//     for (int i = 1; i < s.size(); i++)
+//         s[i - 1] == ' ' ? answer += toupper(s[i]) : answer += tolower(s[i]);
+
+//     return answer;
+// }
